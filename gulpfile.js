1710ca90e9c85099c9afc0f6abbee25ef005a180
Pete Davis <pete@urbanairship.com>
2019-07-05 15:26:57 -0700

    fixed gulp with (bundle clean --force)

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 const gulp = require('gulp');
 const child = require('child_process');
+const browserSync = require('browser-sync');
+const watch = require('gulp-watch');
 
 const jekyll = process.platform === "win32" ? "jekyll.bat" : "jekyll";
+
+var messages = {
+  jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
 
 gulp.task('default', (done) => {
     console.log("Default gulp task is running!");
@@ -13,7 +19,14 @@ gulp.task('jekyll', (done) => {
         '--incremental',
         '--drafts'
     ])
-    .on('close', done);
+    child.exit(1);
 })
+
+gulp.task('jekyll-build', function(done) {
+  browserSync.notify(messages.jekyllBuild);
+  return child.spawn('jekyll', ['build'], {stdio: 'inherit'})
+    .on('close', done);
+});
+
 
 
